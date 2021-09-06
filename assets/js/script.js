8fb4c46232b9d36fe2de685b102c7643fa74e921
alicia chen <aliciachen10@gmail.com>
2021-09-06 02:08:04 -0400

    clean up code and add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,43 +7,29 @@ var textAreas = ['#09block', '#10block', '#11block', '#12block', '#13block', '#1
 '#15block', '#16block', '#17block']
 var timeTextList = []
 
-// var saveButten = $('#save09');
-// saveButten.on('click', saveToLocalStorage);
-
 var myinput = []
-//QUESTION: WHAT IS THE CORRECT WAY TO DO THIS WITH FUNCTION(EVENT, I)
-// for (var i = 0; i < saveButtonIDs.length; i++) {
-//     saveButtonArray[i] = $(saveButtonIDs[i])
-//     saveButtonArray[i].on('click', function(event, i){
-//         event.preventDefault()
-//         myinput[i] = $(textAreas[i]).val()
-//         localStorage.setItem("myinput" + i, JSON.stringify(myinput[i]))
 
-
-//     })
-// }
-
+//create queryselectors for each .saveBtn and set localstorage item myinput+i equal to the content of the textarea
 document.querySelectorAll('.saveBtn').forEach(function(el) {
     el.addEventListener('click', function(event){
         event.preventDefault()
-        console.log(event.target)
-        hourNumber = event.target.id.substring(4,6) //reference to that specific button
-        console.log(hourNumber)
+        hourNumber = event.target.id.substring(4,6)
         hourBlock = "#" + hourNumber + "block"
         myinput[parseInt(hourNumber)-9] = $(hourBlock).val()
         localStorage.setItem("myinput" + (parseInt(hourNumber)-9), JSON.stringify(myinput[parseInt(hourNumber)-9]))
     })
 })
 
+//if myinput+i exists, parse info from myinput+i in localstrage and return that value to the textarea 
 var savedInfoArray = []
 for (var i = 0; i < saveButtonIDs.length; i++){
-//     // console.log(JSON.parse(localStorage.getItem("myinput" + i)))
     if ("myinput" + i){
         savedInfoArray[i] = JSON.parse(localStorage.getItem("myinput" + i)) 
         $(textAreas[i]).val(savedInfoArray[i])
     }
 }
 
+//use clear button to clear all local storage and all text areas 
 var clearBtn = $('.clear-btn')
 clearBtn.on('click', function(event) {
     localStorage.clear()
@@ -52,26 +38,11 @@ clearBtn.on('click', function(event) {
     }
 })
 
+// set currentDay text to current day via moment
 $('#currentDay').text(moment().format("MMMM Do, YYYY"))
-
-
-
-// function saveToLocalStorage(event) {
-//     event.preventDefault()
-//     // var text = $('textarea#mytextarea').val();
-//     myinput = $('#09block').val();
-//     localStorage.setItem('myinput', JSON.stringify(myinput))
-//     // $("#comment").val()
-// };
-//allow the savedinfo to persist over time 
-// var savedInfo = JSON.parse(localStorage.getItem("myinput"));
-// $('#09block').val(savedInfo)
-
-//allow for the textarea to change colors based on if the current hour 
-//    is higher, lower or the same as the one in the cell next to it
 var currentHour = moment().format('H')
-// currentHour = 15;
 
+// for each time block, change its styling/color based on whether it is before, after, or at the current hour
 for (var i = 0; i < timeLabel.length; i++){
     timeTextList[i] = $(timeLabel[i]).text()
     var blockTime = parseInt(textAreas[i].substring(1, 3))
@@ -83,13 +54,3 @@ for (var i = 0; i < timeLabel.length; i++){
         $(textAreas[i]).attr('class', 'future')
     }
 }
-
-//old code
-// var timeText = $('#12time').text()
-// if (currentHour > timeText.substring(0,2)) {
-//     $('#12block').attr('class', 'past')
-// } else if (currentHour == timeText.substring(0,2)) {
-//     $('#12block').attr('class', 'present')
-// } else {   
-//     $('12block').attr('class', 'future')
-// }
